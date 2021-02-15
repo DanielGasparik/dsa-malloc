ebad0fe67edde4c6b4cd565bf58d843b3d710419
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-02-15 18:59:39 +0100

    debugging

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,10 +14,10 @@
 typedef struct mem_block {
     struct mem_block *next;
     struct mem_block *prev;
-    int size;
-    bool free;
+    //int size;
+    //bool free;
 } BLOCK;
-#define block_size sizeof(mem_block)
+#define block_size sizeof(struct mem_block)
 BLOCK *head;
 
 void *memory_alloc(unsigned int size) {
@@ -23,18 +31,25 @@ int memory_free(void *valid_ptr) {}
 int memory_check(void *ptr) {}
 
 void memory_init(void *ptr, unsigned int size) {
+    if (head != NULL) {
+        head = NULL;
+    }
     head = ptr;
-    head->size = size;
-    head->free = true;
+    //head->size = size;
+    //head->free = true;
+    head->prev = NULL;
 
-    printf("%p \n", &*head);
-    printf("%d", head->size);
+    printf("adress of head[0] %p \n adress of ptr[0] %p\n", &*head, &*ptr);
+    printf("block size %d\n",block_size);
+    printf("first available block of memory from head %p \nfirst available block of memory from ptr %p\n", &(*(head + block_size)), &*(ptr + block_size));
 }
 
 int main() {
     char region[50];
-    printf("%p\n", &region);
+
+    printf(" original mempool[0] adress %p\n", &region[0]);
     memory_init(region, 50);
+    printf("original mempool first available adress %p\n", &region[block_size]);
     char *pointer = (char *) memory_alloc(10);
     if (pointer) {
         memset(pointer, 0, 10);
