732825ae3be34c21c7d6030d3d936676f2114923
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-02-15 17:19:30 +0100

    few lines + beautify

--- main.c
+++ main.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct mem_block{
+typedef struct mem_block
+{
     struct mem_block *next;
     struct mem_block *prev;
     int size;
-}BLOCK;
+} BLOCK;
 BLOCK *head;
 
-
-void *memory_alloc(unsigned int size){
-    if(head == NULL){
+void *memory_alloc(unsigned int size)
+{
+    if (head == NULL)
+    {
         return NULL;
     }
-    
 }
-int memory_free(void *valid_ptr){}
-int memory_check(void *ptr){}
-void memory_init(void *ptr, unsigned int size){
+int memory_free(void *valid_ptr) {}
+int memory_check(void *ptr) {}
+void memory_init(void *ptr, unsigned int size)
+{
     head = ptr;
+    printf("%p \n", &*head);
 }
-int main() {
+int main()
+{
     char region[50];
-    memory_init(region,50);
-    char *pointer = (char*)memory_alloc(10);
-    if (pointer){
-        memset(pointer,0,10);
-
+    printf("%p\n", &region);
+    memory_init(region, 50);
+    char *pointer = (char *)memory_alloc(10);
+    if (pointer)
+    {
+        memset(pointer, 0, 10);
     }
-    if(pointer){
+    if (pointer)
+    {
         memory_free(pointer);
     }
     return 0;
