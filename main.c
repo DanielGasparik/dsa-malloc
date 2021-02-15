b4e22c4155fbe5710fdef3d896b228557dea530e
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-02-15 16:51:00 +0100

    added main.c into the staging setup of the project done

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct mem_block{
+    struct mem_block *next;
+    struct mem_block *prev;
+    int size;
+}BLOCK;
+BLOCK *head;
+
+
+void *memory_alloc(unsigned int size){}
+int memory_free(void *valid_ptr){}
+int memory_check(void *ptr){}
+void memory_init(void *ptr, unsigned int size){
+    head = ptr;
+}
+int main() {
+    char region[50];
+    memory_init(region,50);
+    char *pointer = (char*)memory_alloc(10);
+    if (pointer){
+        memset(pointer,0,10);
+
+    }
+    if(pointer){
+        memory_free(pointer);
+    }
+    return 0;
+}
