94a4a168fb21981c9d193781087f194a07349958
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-03-05 17:51:25 +0100

    first tests

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 typedef struct mem_block {
     void *next;
@@ -13,13 +12,16 @@ void *head = NULL;
 //TODO find free
 
 void iterate() {
-    BLOCK *iterator = (BLOCK *) head;
-    while (iterator != NULL) {
-        printf("My adress: %d\naddress of my payload: %d\nMy size %d\n Am I free?: %s\nSize of my payload %d", iterator,
-               iterator + block_size, block_size, iterator->size > 0 ? "NO" : "YES",
-               iterator->size < 0 ? iterator->size * -1 : iterator->size);
+    void *iterator = (BLOCK *) head;
+    int i = 1;
+    while ((BLOCK *) iterator != NULL) {
+        printf("\nI am header %d\nMy adress: %d\nAddress of my payload: %d\nMy size %d\nAm I free?: %s\nSize of my payload %d\n",
+               i, iterator,
+               iterator + block_size, block_size, ((BLOCK *) iterator)->size > 0 ? "NO" : "YES",
+               ((BLOCK *) iterator)->size < 0 ? ((BLOCK *) iterator)->size * -1 : ((BLOCK *) iterator)->size);
 
-        iterator = iterator->next;
+        iterator = ((BLOCK *) iterator)->next;
+        i++;
     }
 }
 
@@ -37,36 +39,17 @@ void *find_free_block(unsigned int size) {
 
     return NULL;
 
-    /* BLOCK* tmp1 = (BLOCK*)head;
-    BLOCK* tmp2 = ((BLOCK*)head)->next;
-    BLOCK* best_fiit = NULL;
-    unsigned int rozdiel_best = -1, rozdiel_temp;
-    while(tmp2->next!=NULL){
-
-        if(tmp2->size>=size){
-            rozdiel_temp = tmp2->size-size;
-            if (rozdiel_best == -1){rozdiel_best == rozdiel_temp;}
-            if(rozdiel_temp<=rozdiel_best){
-                best_fiit = tmp2;
-            }
-
-        }
-        tmp1 = tmp2;
-        tmp2 = tmp2->next;
-    }
-    tmp1->next = tmp2->next;
-    tmp2->next = NULL;
-    printf("adresa noveho bloku bez payload %d\n ",&*best_fiit);
-    return best_fiit;*/
 }
 
 void *memory_alloc(unsigned int size) {
     if (head == NULL || size <= 0) {
+        printf("Size is smaller than 0 or hea points to NULL(bad initialization)\n");
         return NULL;
     }
     void *free = find_free_block(size);
     //no free blocks left - memory full
     if (free == NULL) {
+        printf("Not enough room to allocate a ptr of size %d\n", size);
         return NULL;
     } else {
         //free->next = free+block_size+size;
@@ -78,9 +61,10 @@ void *memory_alloc(unsigned int size) {
         next->prev = ((BLOCK *) free);
 
     }
-    printf("Adresa mallocovaneho void %d\n",free);
-    printf("Adresa mallocovaneho void+1 %d\n",free+1);
-    printf("Adresa mallocovaneho void+blocksize %d\n",free+block_size);
+    printf("Podarilo sa mi alokovat o velkosti %d, na adrese %d\n",size,free+block_size);
+    //printf("Adresa mallocovaneho void+1 %d\n", free + 1);
+    //printf("Adresa mallocovaneho void+blocksize %d\n", free + block_size);
+
     return free + block_size;
     //return free+block_size;
 
@@ -95,6 +79,7 @@ int memory_free(void *valid_ptr) {
     valid_ptr -= block_size;//zaciatok hlavicky;
     BLOCK *help = valid_ptr;
     if (help->size < 0) {
+        printf("Pointer beeing freed is already free");
         return 1;//nemame co odalokovat
     }
     help->size *= -1;
@@ -120,41 +105,32 @@ int memory_free(void *valid_ptr) {
 int memory_check(void *ptr) {
     BLOCK *help = head;
     void *iterator;
-    if (head == NULL || ptr == NULL) {
+    if (head == NULL || ptr == NULL || ptr <= head) {
         return 0;
     }
 
-
-    for (iterator = help; iterator <= help->size + block_size; iterator++) {
-        if (iterator == help->next) {
-            help = help->next;
-        }
-        if (iterator == ptr) {
-            if(iterator>=help&&iterator<=help+block_size){return 0;}
-            if (help->size < 0) { return 0; }
-        }
-    }
-    /*
     while (help != NULL) {
-        for (iterator = help; iterator < (help->size) + block_size; iterator++) {
+        void *help2 = help;
+        for (iterator = help; iterator <= help2 + help->size + block_size; iterator++) {
             if (iterator == ptr) {
-                if(iterator>=help&&iterator<=help+block_size){
+                if (iterator >= help2 && iterator < help2 + block_size) {
+                    printf("Pointer is in the head\n");
                     return 0;
                 }
                 if (help->size < 0) {
+                    printf("Pointer is already free\n");
                     return 0;
                 }
             }
         }
         help = help->next;
-
-    }*/
-    /*
-    BLOCK *check = ptr - block_size;
-    if (!check || check->size < 0) {
+    }
+    if (ptr > iterator) {
+        printf("Pointer passed to memory check is out of bounds to right\n");
         return 0;
     }
-    */
+
+
     return 1;
 }
 
@@ -224,7 +200,7 @@ void z1_testovac(char *region, char **pointer, int minBlock, int maxBlock, int m
     }
     for (int j = 0; j < i; j++) {
         if (memory_check(pointer[j])) {
-            //memory_free(pointer[j]);
+            memory_free(pointer[j]);
         } else {
             printf("Error: Wrong memory check.\n");
         }
@@ -241,6 +217,7 @@ void z1_testovac(char *region, char **pointer, int minBlock, int maxBlock, int m
     printf("Memory size of %d bytes: allocated %.2f%% blocks (%.2f%% bytes).\n", random_memory, result, result_bytes);
 }
 
+/*
 int main() {
     char region[100000];
     char *pointer[13000];
@@ -248,28 +225,136 @@ int main() {
     z1_testovac(region, pointer, 8, 1000, 10000, 20000, 0);
     z1_testovac(region, pointer, 8, 35000, 50000, 99000, 0);
     return 0;
+}*/
+int rand_int(int min, int max) {
+    return (rand() % (max + 1 - min) + min);
+}
+void frag_printout(int size){
+    double hlavicka_count = 0;
+    BLOCK *iterator = head;
+    double obsadene_bytes = 0;
+    while (iterator != NULL) {
+        if(iterator->size>0){
+            obsadene_bytes+=iterator->size;
+        }
+        hlavicka_count++;
+        iterator = iterator->next;
+
+    }
+    double frag = (hlavicka_count*block_size)/size;
+    double zaplnilo = obsadene_bytes/size;
+    printf("Fragmentacia je %.2f%%\nZaplnene je %.2f%%\n",frag*100,zaplnilo*100);
+}
+void test1() {
+
+    char region1[50];
+    char region2[100];
+    char region3[200];
+    char *pointer_arr1[2];
+    char *pointer_arr2[4];
+    char *pointer_arr3[8];
+    memory_init(region1, 50);
+    int j = 0;
+    printf("TEST1a\n");
+    printf("Region size = 50\nHead size = %d\nMaximum size you can alloc: %d\n", block_size, 50 - (2 * block_size) - 1);
+    int a = 8;
+    for (int i = 0; i < 2; i++) {
+        pointer_arr1[i] = memory_alloc(a);
+    }
+    frag_printout(50);
+    iterate();
+
+    //ak len alokujem
+    //spocitam si vsetky hlavicky
+    //kolko z celkovej alokacie tvoria hlavicky
+    //toto je framgentácia
+
+    //5 hlaviciek
+    // a 5 blokov
+    //Bloky co som alokoval(5) / cela velkost je % co sa mi podarilo zaplnit
+    a = 11;
+    for (int i = 0; i < 2; i++) {
+        pointer_arr1[i] = memory_alloc(a);
+        if (i % 2 == 0) {
+            if (memory_check(pointer_arr1[i])) {
+                memory_free(pointer_arr1[i]);
+            }
+        }
+    }
+    a = 21;
+    for (int i = 0; i < 2; i++) {
+        pointer_arr1[i] = memory_alloc(a);
+        if (i % 2 == 0) {
+            if (memory_check(pointer_arr1[i])) {
+                memory_free(pointer_arr1[i]);
+            }
+        }
+    }
+
+
+    memory_init(region2, 100);
+    j = 0;
+    printf("\n\nTEST1b\n");
+    printf("Region size = 100\nHead size = %d\nMaximum size you can alloc: %d\n", block_size, 100 - (2 * block_size));
+    for (int i = 0; i < 4; i++) {
+        pointer_arr2[i] = memory_alloc(12);
+
+        if (i == 2) {
+            if (memory_check(pointer_arr1[i])) {
+                memory_free(pointer_arr1[i]);
+            }
+        }
+    }
+    iterate();
+    frag_printout(100);
+
+
+    memory_init(region3, 200);
+    j = 0;
+    printf("\n\nTEST1c\n");
+    printf("Region size = 200\nHead size = %d\nMaximum size you can alloc: %d\n", block_size, 200 - (2 * block_size));
+    for (int i = 0; i < 8; i++) {
+        pointer_arr3[i] = memory_alloc(24);
+
+        if (i == 2) {
+            if (memory_check(pointer_arr1[i])) {
+                memory_free(pointer_arr1[i]);
+            }
+        }
+    }
+    iterate();
+    frag_printout(200);
+
+
+
+
 }
 
-/*int main() {
+void test2() {
+
+}
+
+void test3() {
+
+}
+
+void test4() {
+
+}
+
+
+int main() {
+    srand(time(0));
     char region[100];
 
-    //printf(" original mempool[0] adress %d\n", &region[0]);
+    test1();
+    test2();
+    test3();
+    test4();
     memory_init(region, 100);
-    //printf("adresa headu %d\n next free block %d\n size left after main head and first free block head %d\n",head, ((BLOCK*)head)->next, ((BLOCK*)head)->next->size);
-    //printf("original mempool first available adress %d\n", &region[block_size]);
+
     char *pointer = (char *) memory_alloc(10);
-    iterate();
-    free(pointer);
-    char *pointer2 = (char *) memory_alloc(7);
-    iterate();
-    char*pointer3 = (char*) memory_alloc("8");
-    iterate();
-    free(pointer3);
-    iterate();
-   // BLOCK *abc = ((BLOCK *) head)->next;
-    //printf("%d\n", block_size);
-    //printf("adresa sucasneho %d velkost sucasneho %d\n adresa dalsieho %d velkost dalsieho %d\n", head,
-          // ((BLOCK *) head)->size, abc, abc->size);
+
     if (pointer) {
         memset(pointer, 0, 10);
     }
@@ -278,4 +363,3 @@ int main() {
     }
     return 0;
 }
- */
